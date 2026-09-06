495ce03e6dd2fcaeb61e87df874d20b1fd8a0f6b
Manan Puri <mpu3450@gmail.com>
2026-09-06 18:19:39 +0530

    update

--- file.py
+++ file.py
@@ -4,4 +4,4 @@ st.subheader("CAREGIVER LOGIN")
 st.text_input('Email Address')
 st.text_input("password",type = "password")
 if st.button("login"):
-    st.switch_page("pages/dashboard.py")
+    st.switch_page("frontend/dashboard.py")
